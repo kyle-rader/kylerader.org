2cf9e46e314c7a43cf9592b93ed2caf2e5b5bc0d
kyle-rader <kyle.rader@ieee.org>
2014-08-18 22:50:34 -0700

    Initial media added

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<head>
+    <title>Kyle W. Rader</title>
+    <link rel="stylesheet" type="css/text" href="/css/reboot.css"/>
+    <link rel="stylesheet" type="css/text" href="/css/normalize.css"/>
+    <link rel="stylesheet" type="css/text" href="/css/foundation.min.css"/>
+    <link rel="stylesheet" type="css/text" href="/f-icons/foundation-icons.css"/>
+    <link rel="stylesheet" type="css/text" href="/css/main.css"/>
+
+</head>
+<body>
+
+</body>
+</html>
