351c367641e7f3d119a20ccf2a566f3c0a81e974
kyle-rader <kyle.rader@ieee.org>
2014-09-07 01:00:36 -0700

    Add Hackathon and CLassfinder structures.

---
+++ Classfinder/content.php
@@ -0,0 +1,19 @@
+<div id="mainWrapper" class="dark-bg">
+    <div id="header" >
+        <div class="row center-text">
+            <div class="small-12 columns">
+                <h1>A better way to find your classes.</h1>
+            </div>
+        </div>
+    </div>
+    <div id="content">
+        <div class="row center-text">
+            <div class="small-12 columns">
+                <h3>Under Construction :/</h3>
+            </div>
+        </div>
+    </div>
+    <div id="footer">
+        Footer
+    </div>
+</div>
---
+++ Hack/content.php
@@ -0,0 +1,21 @@
+<div id="mainWrapper" class="dark-bg">
+    <div id="header" >
+        <div class="row center-text">
+            <div class="small-12 columns">
+                <h1>WWU Fall Hackathon!</h1>
+            </div>
+        </div>
+    </div>
+    <div id="content">
+        <div class="row dark-bg">
+            <div class="small-12 columns">
+                <ul>
+                    <li><strong>When:</strong>&nbsp; Nov 1st 11:00 AM - Nov 2nd 2:00 PM</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div id="footer">
+        Footer
+    </div>
+</div>
