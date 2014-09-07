86d6d02a509a1ff2d4d8e107b0cc307db0b9f30e
kyle-rader <kyle.rader@ieee.org>
2014-09-07 03:49:34 -0700

    Add hackathon info.

--- Hack/content.php
+++ Hack/content.php
@@ -1,6 +1,6 @@
 <div id="mainWrapper" class="dark-bg">
     <div id="header" >
-        <div class="row center-text dark-bg">
+        <div class="row center-text">
             <div class="small-12 columns">
                 <h1>WWU Fall Hackathon!</h1>
             </div>
@@ -10,10 +10,40 @@
         <div class="row dark-bg">
             <div class="small-12 columns">
                 <ul>
-                    <li><strong>When:</strong>&nbsp; Nov 1st 11:00 AM - Nov 2nd 2:00 PM</li>
+                    <li><h4 class="green-text">Who</h4>Western Washington University & Whatcom Community College students!</li>
+                    <li><h4 class="green-text">When</h4>Nov 1st 11:00 AM - Nov 2nd 2:00 PM</li>
+                    <li><h4 class="green-text">Where</h4>WWU Computer Science Department & Labs</li>
+                    <li><h4 class="green-text">Why</h4>
+                    The Hackathon has two primary functions.  First and foremost we want to get students out of their
+                    books and into a team building an actual thing!  Second, we want to connect you with companies interested
+                    in hiring you!  Multiple companies will be participating and sponsoring the event so take advantage of
+                    their interest!
+                    </li>
+                    <li><h4 class="green-text">How</h4>
+                    You will form a team prior to or at the start of the hackathon.  Your team must have an idea for an app
+                    (mobile or desktop), website, service, etc that would be useful to students/faculty/staff at our your school.
+                    You will have exactly 24 hours to "hack-together" (hence:hackathon) a prototype or a full fledged app.  At the
+                    end your team must present to a panel of judges. You have exactly 3 minutes of presentation followed by 3 minutes
+                    of Q & A.<br>
+                    </li>
+                    <li><span class="green-text">TLDR:</span>
+                    You.  Form team.  Build app/website/service in 24 hours.  Pitch it.  Win cash prizes! Network with companies!
+                    </li>
                 </ul>
             </div>
         </div>
+        <div class="row dark-bg">
+            <div class="small-12 columns">
+                <h4>Register Now!</h4>
+            </div>
+        </div>
+        <div class="row dark-bg">
+            <div class="small-6 columns">
+                <a class="button" href="#">Team Registration</a>
+            </div>
+            <div class="small-6 columns">
+                <a class="button" href="#">Company Registration</a>
+            </div>
     </div>
     <div id="footer">
         <?php include_once "$docRoot/include/footer.php"; ?>
