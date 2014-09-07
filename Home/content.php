a430d2c80bf5f5eb1f60f8ac429339a85a613ae7
kyle-rader <kyle.rader@ieee.org>
2014-09-07 02:03:44 -0700

    Add video to homepage.  User HeroLight on all h# elements.

--- Home/content.php
+++ Home/content.php
@@ -6,13 +6,23 @@
             </div>
         </div>
     </div>
+
     <div id="content">
         <div class="row center-text">
             <div class="small-12 columns">
-                <h3>"People don't buy what you do they buy why you do it!" - Simon Sinek</h3>
+                <h3>Enjoy one of my favorite TED talks</h3>
+                <hr>
+            </div>
+        </div>
+        <div class="row center-text">
+            <div class="small-12 columns">
+                <div class="flex-video widescreen vimeo">
+                    <iframe src="https://embed-ssl.ted.com/talks/benjamin_zander_on_music_and_passion.html" width="854" height="480" frameborder="0" scrolling="no" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
+                </div>
             </div>
         </div>
     </div>
+
     <div id="footer">
         <?php include_once "$docRoot/include/footer.php"; ?>
     </div>
