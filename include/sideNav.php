fe3e8931857c6ecfc9239c8f8403971cbce08ccd
kyle-rader <kyle.rader@ieee.org>
2014-09-07 12:46:45 -0700

    Small screen menu styling.

--- include/sideNav.php
+++ include/sideNav.php
@@ -1,5 +1,5 @@
-<nav class="SideNav dark-bg">
-    <ul>
+<nav class="SideNav">
+    <ul class="show-for-medium-up">
     <?php
         foreach($terms['nav-items'] as $url => $name) {
             $active = strtoupper($pageTitle) == $name ? 'class="active"' : '';
@@ -7,4 +7,15 @@
         }
     ?>
     </ul>
+    <div class="mobile-menu hide-for-medium-up">
+        <a href="#" data-dropdown="mobile-menu"><i class="fi-list"></i></a>
+        <ul id="mobile-menu" class="mega f-dropdown" data-dropdown-content>
+            <?php
+            foreach($terms['nav-items'] as $url => $name) {
+                $active = strtoupper($pageTitle) == $name ? 'class="active"' : '';
+                print "<li><a $active href=\"$url\">$name</a></li>";
+            }
+            ?>
+        </ul>
+    </div>
 </nav>
