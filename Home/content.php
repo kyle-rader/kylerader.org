a01a2192d28eb124886259fb8148f15a275b0afd
kyle-rader <kyle.rader@ieee.org>
2014-09-07 00:23:34 -0700

    Finally decided on a background and color scheme.

--- Home/content.php
+++ Home/content.php
@@ -1,19 +1,19 @@
-<div id="mainWrapper">
-    <div id="header" class="dark-bg">
-        <div class="row">
+<div id="mainWrapper" class="dark-bg">
+    <div id="header" >
+        <div class="row center-text">
             <div class="small-12 columns">
                 <h1>You have reached Kyle's website.</h1>
             </div>
         </div>
     </div>
     <div id="content">
-        <div class="row">
+        <div class="row center-text">
             <div class="small-12 columns">
                 <h3>"People don't buy what you do they buy why you do it!" - Simon Sinek</h3>
             </div>
         </div>
     </div>
-    <div id="footer" class="dark-bg">
+    <div id="footer">
         Footer
     </div>
 </div>
