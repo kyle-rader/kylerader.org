c57a04ae68f4bc346755be7b4f4e4ceb76d19963
kyle-rader <kyle.rader@ieee.org>
2014-09-07 10:44:57 -0700

    Add medium columns to Home.

--- Home/content.php
+++ Home/content.php
@@ -9,12 +9,12 @@
 
     <div id="content">
         <div class="row center-text">
-            <div class="small-12 columns">
+            <div class="small-12 medium-8 medium-centered columns">
                 <h3>Enjoy one of my favorite TED talks</h3>
             </div>
         </div>
         <div class="row center-text">
-            <div class="small-12 columns">
+            <div class="small-12 medium-8 medium-centered columns">
                 <div class="flex-video widescreen">
                     <iframe width="853" height="480" src="https://www.youtube-nocookie.com/embed/qp0HIF3SfI4?rel=0" frameborder="0" allowfullscreen></iframe>
                 </div>
