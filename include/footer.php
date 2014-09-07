eb1d67f284845f8c204a786b95d45cc1e290c390
kyle-rader <kyle.rader@ieee.org>
2014-09-07 02:52:57 -0700

    Add social icon link hrefs

--- include/footer.php
+++ include/footer.php
@@ -1,11 +1,11 @@
 <div class="large-4 columns">
     <ul class="icon-list">
-        <li><a href="#"><i class="fi-social-linkedin social-icon"></i></a></li>
-        <li><a href="#"><i class="fi-social-github social-icon"></i></a></li>
-        <li><a href="#"><i class="fi-social-facebook social-icon"></i></a></li>
-        <li><a href="#"><i class="fi-social-twitter social-icon"></i></a></li>
-        <li><a href="#"><i class="fi-social-google-plus social-icon"></i></a></li>
-        <li><a href="#"><i class="fi-mail social-icon"></i></a></li>
+        <li><a href="https://www.linkedin.com/pub/kyle-rader/7b/361/882/"><i class="fi-social-linkedin social-icon"></i></a></li>
+        <li><a href="https://github.com/kyle-rader"><i class="fi-social-github social-icon"></i></a></li>
+        <li><a href="https://www.facebook.com/Mcyamaha818"><i class="fi-social-facebook social-icon"></i></a></li>
+        <li><a href="https://twitter.com/Kyle_W_Rader"><i class="fi-social-twitter social-icon"></i></a></li>
+        <li><a href="https://plus.google.com/u/0/106763793760218207085/posts/p/pub"><i class="fi-social-google-plus social-icon"></i></a></li>
+        <li><a href="mailto:kyle.rader+fromWeb@ieee.org"><i class="fi-mail social-icon"></i></a></li>
     </ul>
 </div>
 <div class="large-4 columns">
