e6a6705ed431f009ffe9b8c7477c12e30db3f163
kyle-rader <kyle.rader@ieee.org>
2014-09-09 10:05:31 -0700

    Add database config.

--- include/db.php
+++ include/db.php
@@ -1,2 +1,7 @@
 <?php
+$mysql_ini = parse_ini_file("$docRoot/.config/mysql.ini");
+$dbHostName = $mysql_ini['server'];
+$dbUserName = $mysql_ini['username'];
+$dbPassword = $mysql_ini['password'];
+$dbDatabaseName = $mysql_ini['database'];
 
