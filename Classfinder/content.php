0129227df1390e3eecedd6d4ed392a81c13b6e00
kyle-rader <kyle.rader@ieee.org>
2014-09-23 11:05:55 -0700

    Update tagline and button transitions

--- Classfinder/content.php
+++ Classfinder/content.php
@@ -3,10 +3,8 @@
 		<br>
 		<br>
 		<div class="unit-100">
-			<h1>Take</h1>
-			<h1>Control</h1>
-			<h1>of Your</h1>
-			<h1>Classes</h1>
+			<h1>Classfinder is so awesome!</h1>
+			<h1>Said no one ever...</h1>
 		</div>
 	</div>
 </div>
