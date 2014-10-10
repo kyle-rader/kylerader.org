8b639f5528360640f36c1a9e57d5dac309532a03
kyle-rader <kyle.rader@ieee.org>
2014-10-10 12:36:14 -0700

    Add calendar to homepage.

--- Home/content.php
+++ Home/content.php
@@ -7,4 +7,13 @@
         	</div>
     	</div>
     </div>
+    <div class="units-row">
+    	<div class="unit-50 unit-centered">
+    		<h3>Need Kyle?</h3>
+    		<h3>If its not on my calendar it isn't happening.</h3>
+    		<iframe src="https://www.google.com/calendar/embed?mode=WEEK&amp;height=600&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=kyle.rader%40ieee.org&amp;color=%2329527A&amp;ctz=America%2FLos_Angeles" 
+    		style=" border-width:0 " width="800" height="600" frameborder="0" scrolling="no">
+    		</iframe>
+    	</div>
+    </div>
 </div>
