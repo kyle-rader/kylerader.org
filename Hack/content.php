3ceed5bd58a493079034d4ae816e986e8f86fb5c
kyle-rader <kyle.rader@ieee.org>
2014-10-22 09:19:19 -0700

    Update Hackathon page.

--- Hack/content.php
+++ Hack/content.php
@@ -1,36 +1,9 @@
 <div class="units-row">
     <div class="unit-80 unit-centered">
         <div class="units-row">
-            <div class="unit-60">
-                <ul class="list-flat list-spaced">
-                    <li><h4 class="blue-text bold">Who</h4>WWU, WCC, and BTC students!</li>
-                    <li><h4 class="blue-text bold">When</h4><strong>Nov 1st</strong> 11:00 AM - <strong>Nov 2nd </strong> 2:00 PM</li>
-                    <li><h4 class="blue-text bold">Where</h4>WWU Computer Science Department & Labs (Communications Facility 1st & 4th floors)</li>
-                    <li><h4 class="blue-text bold">Why</h4>
-                    We want to get students out of their books and into a team creating and building something!  
-                    This event connects you with companies interested in hiring you!  Several companies will be participating and 
-                    sponsoring the event so take advantage of their interest!  There will be free food, conversation, and swag!
-                    </li>
-                    <li><h4 class="blue-text bold">How</h4>
-                    Form a team prior to or preferably at the start of the hackathon.  Your team must have an idea for an application
-                    that would be useful to students/faculty/staff at your school.  It can be for any platform (mobile, desktop, web, command line, etc)
-                    You will have exactly 24 hours to build a prototype or a full fledged app and practice pitching it.  At the
-                    end your team will present to a panel of judges. You have exactly 4 minutes of presentation and 3 minutes
-                    of Q & A.<br>
-                    </li>
-                </ul>
-            </div>
-            <div class="unit-40">
+            <div class="unit-50">
                 <h4 class="blue-text bold">Pre-Events</h4>
                 <ul class="list-flat list-spaced-large">
-                    <li>
-                        <h5>Hackathon Information Session <strong class="red-text">2</strong></h5>
-                        <strong>Oct 20 - 5:30 pm</strong> | WWU Communication Facility <strong class="red-text">105 (note change)</strong>
-                    </li>
-                    <li>
-                        <h5>Git Version Control Lecture</h5>
-                        <strong>Oct 21 - 5:00 pm</strong> | WWU Communication Facility <strong>125</strong>
-                    </li>
                     <li>
                         <h5>Git Lab Session 1 (25 seats)</h5>
                         <strong>Oct 22 - <span class="red-text">4:00 pm</span></strong> | WWU Communication Facility <strong>162</strong>
@@ -44,26 +17,14 @@
                         <strong>Oct 30 - 5:00 pm</strong> | WWU Communication Facility <strong>162</strong>
                     </li>
                 </ul>
-                <h4 class="blue-text bold">Sponsors</h4>
-                <ul class="blocks-3 logos">
-                    <li><img src="/img/er_logo.png" alt="Emergency Reporting"></li>
-                    <li><img src="/img/faithlife_logo.jpg" alt="Faithlife"></li>
-                    <li><img src="/img/vitech_logo.png" alt="Vitech Group"></li>
-                    <li class="block-first"><img src="/img/dis_logo.png" alt="Dealer Information Systems"></li>
-                    <li><img src="/img/slalom_logo.png" alt="Slalom Consulting"></li>
-                    <li><img src="/img/spie_logo.png" alt="SPIE"></li>
+            </div>
+            <div class="unit-50">
+                <ul class="list-flat list-spaced-large">
+                    <li><a class="btn btn-blue" href="/Hack/Register">Register</a></li>
+                    <li><a class="btn btn-blue" href="/Hack/TeamRegistration">Make a Team</a></li>
+                    <li><a class="btn btn-blue" href="/Hack/CompanyRegistration">Company Registration</a></li>
+                    <li>Questions? <a href="mailto:kyle.rader+hackathon@ieee.org">Email Kyle!</a></li>
                 </ul>
-            </div>
-        </div>
-        <div class="units-rows">
-            <div class="unit-60">
-                <h5 style="display: inline-block; vertical-align:5px;">Step 1</h5><i class="fi-arrow-right" style="padding: 5px 10px 0px 10px; display:inline-block; font-size:2em;"></i>
-                <a class="btn btn-blue" href="/Hack/TeamRegistration">Make a Team</a>
-                <h5 style="display: inline-block; vertical-align:5px;">&nbsp;Step 2</h5><i class="fi-arrow-right" style="padding: 5px 10px 0px 10px; display:inline-block; font-size:2em;"></i>
-                <a class="btn btn-blue" href="/Hack/Register">Register</a>
-            </div>
-            <div class="unit-40">
-                <a class="btn btn-blue right" href="/Hack/CompanyRegistration">Company Registration</a>
             </div>
         </div>
     </div>
