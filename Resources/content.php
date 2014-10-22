6a2f7fb8b3effa8b1b407eba4d569bcce36199f2
kyle-rader <kyle.rader@ieee.org>
2014-10-22 08:13:45 -0700

    Add link to git workflow presentation

--- Resources/content.php
+++ Resources/content.php
@@ -1,41 +1,46 @@
 <div class="units-row">
     <div class="unit-80 unit-centered">
-        <ul class="blocks-3">
+        <ul class="blocks-4">
+	    <li>
+	        <h4>From Kyle</h4><hr>
+		<ul class="list-flat">
+		    <li><a target="_blank" href="https://docs.google.com/presentation/d/1fJsNiXxPP82q3OeNOvui_IpTUl9Y10dnW2AWTRo8UIs/edit?usp=sharing">Topic Branch git Workflow</a></li>
+		</ul>
             <li>
                 <h4>Coding</h4><hr>
                 <ul class="list-flat">
-                    <li><a class="button small" target="_blank" href="https://www.codeschool.com/">Code School</a></li>
-                    <li><a class="button small" target="_blank" href="http://www.tutorialspoint.com/computer_programming_tutorials.htm">Tutorials Point</a></li>
-                    <li><a class="button small" target="_blank" href="http://www.codecademy.com/">Codecademy</a></li>
-                    <li><a class="button small" target="_blank" href="http://code.org/">Code.org</a></li>
-                    <li><a class="button small" target="_blank" href="http://girldevelopit.com/">Girl Develop It</a></li>
-                    <li><a class="button small" target="_blank" href="https://www.udacity.com/course/cs101">Stanford Intro to CS</a></li>
-                    <li><a class="button small" target="_blank" href="https://coderdojo.com/">Code Dojo</a></li>
-                    <li><a class="button small" target="_blank" href="http://nodeschool.io/">Node School</a></li>
+                    <li><a  target="_blank" href="https://www.codeschool.com/">Code School</a></li>
+                    <li><a  target="_blank" href="http://www.tutorialspoint.com/computer_programming_tutorials.htm">Tutorials Point</a></li>
+                    <li><a  target="_blank" href="http://www.codecademy.com/">Codecademy</a></li>
+                    <li><a  target="_blank" href="http://code.org/">Code.org</a></li>
+                    <li><a  target="_blank" href="http://girldevelopit.com/">Girl Develop It</a></li>
+                    <li><a  target="_blank" href="https://www.udacity.com/course/cs101">Stanford Intro to CS</a></li>
+                    <li><a  target="_blank" href="https://coderdojo.com/">Code Dojo</a></li>
+                    <li><a  target="_blank" href="http://nodeschool.io/">Node School</a></li>
                 </ul>
             </li>
             <li>
                 <h4>Cool Things</h4><hr>
                 <ul class="list-flat">
                     <li><a href="http://www.ndpsoftware.com/git-cheatsheet.html#loc=remote_repo;">Best Git Cheatsheet</a></li>
-                    <li><a class="button small" target="_blank" href="http://www.synapse.com/wall-of-cool">All the Cool Things Are Here</a></li>
-                    <li><a class="button small" target="_blank" href="http://xamarin.com/">Xamarin</a></li>
-                    <li><a class="button small" target="_blank" href="http://d3js.org/">D3.js</a></li>
-                    <li><a class="button small" target="_blank" href="http://sass-lang.com/">Sass</a></li>
-                    <li><a class="button small" target="_blank" href="http://coffeescript.org/">Coffee Script</a></li>
-                    <li><a class="button small" target="_blank" href="http://phonegap.com/">Phone Gap</a></li>
-                    <li><a class="button small" target="_blank" href="http://www.nand2tetris.org/">Nand to Tetris</a></li>
-                    <li><a class="button small" target="_blank" href="https://koding.com/Activity">Koding</a></li>
+                    <li><a  target="_blank" href="http://www.synapse.com/wall-of-cool">All the Cool Things Are Here</a></li>
+                    <li><a  target="_blank" href="http://xamarin.com/">Xamarin</a></li>
+                    <li><a  target="_blank" href="http://d3js.org/">D3.js</a></li>
+                    <li><a  target="_blank" href="http://sass-lang.com/">Sass</a></li>
+                    <li><a  target="_blank" href="http://coffeescript.org/">Coffee Script</a></li>
+                    <li><a  target="_blank" href="http://phonegap.com/">Phone Gap</a></li>
+                    <li><a  target="_blank" href="http://www.nand2tetris.org/">Nand to Tetris</a></li>
+                    <li><a  target="_blank" href="https://koding.com/Activity">Koding</a></li>
                 </ul>
             </li>
             <li>
                 <h4>Western</h4><hr>
                 <ul class="list-flat">
-                    <li><a class="button small" target="_blank" href="https://cse.wwu.edu/computer-science">WWU Computer Science</a></li>
-                    <li><a class="button small" target="_blank" href="https://support.cs.wwu.edu/index.php/Main_Page">CS Support</a></li>
-                    <li><a class="button small" target="_blank" href="http://sw.cs.wwu.edu/~nordwap/">CS Web Cams</a></li>
-                    <li><a class="button small" target="_blank" href="http://www.wwu.edu/wwuarchitect/pages/fd/pdf/campusfloorplans/CF04W001.pdf">Dept 4th Floor</a></li>
-                    <li><a class="button small" target="_blank" href="http://www.wwu.edu/wwuarchitect/pages/fd/pdf/campusfloorplans/CF01W001.pdf">Dept 1st Floor</a></li>
+                    <li><a  target="_blank" href="https://cse.wwu.edu/computer-science">WWU Computer Science</a></li>
+                    <li><a  target="_blank" href="https://support.cs.wwu.edu/index.php/Main_Page">CS Support</a></li>
+                    <li><a  target="_blank" href="http://sw.cs.wwu.edu/~nordwap/">CS Web Cams</a></li>
+                    <li><a  target="_blank" href="http://www.wwu.edu/wwuarchitect/pages/fd/pdf/campusfloorplans/CF04W001.pdf">Dept 4th Floor</a></li>
+                    <li><a  target="_blank" href="http://www.wwu.edu/wwuarchitect/pages/fd/pdf/campusfloorplans/CF01W001.pdf">Dept 1st Floor</a></li>
                 </ul>
             </li>
         </ul>
